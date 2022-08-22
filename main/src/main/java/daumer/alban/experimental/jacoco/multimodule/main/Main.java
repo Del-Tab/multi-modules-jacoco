7973c4fed68900d87c79b817358e431b55a6ac74
DelTab <daumera@gmail.com>
2022-08-22 23:02:30 +0200

    dumb project with one main class

---
+++ main/src/main/java/daumer/alban/experimental/jacoco/multimodule/main/Main.java
@@ -0,0 +1,7 @@
+package daumer.alban.experimental.jacoco.multimodule.main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
